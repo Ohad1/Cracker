9917ad13ac94a1eee8a4122adffd428f4a186a85
Ohad <ohad.gabbay@gmail.com>
2022-07-24 21:27:16 +0300

    Configure config.json path

--- Cracker/cracker_app.py
+++ Cracker/cracker_app.py
@@ -1,10 +1,13 @@
 import json
 import multiprocessing
 from Runners.run_master import run_master
+from pathlib import Path
+import os
 
 
 def cracker_app():
-    with open('../config.json') as config_json:
+    config_path = os.path.join(Path(__file__).parent.parent, 'config.json')
+    with open(config_path) as config_json:
         config = json.load(config_json)
     master_proc = multiprocessing.Process(target=run_master, args=('master', config))
     master_proc.start()
